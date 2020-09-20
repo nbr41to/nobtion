5bf97bc7a3b18997740267555c6c7dcdea5858b0
nbr41to <nbr.41to@gmail.com>
2020-09-20 13:44:50 +0900

    styled-componentsできた

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-class MyDocument extends Document {
+type Props = {
+  styleTags: any
+}
+
+class MyDocument extends Document<Props> {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet()
+
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    )
+
+    const styleTags = sheet.getStyleElement()
+
+    return { ...page, styleTags }
+  }
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+          {this.props.styleTags}
+        </Head>
         <body>
           <Main />
           <NextScript />
