7db1c6e60ae36d5d744e9fd7b59263f51d63ffbc
nbr41to <nbr.41to@gmail.com>
2020-09-21 17:11:04 +0900

    Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import '../styles/reset.css'
 import 'katex/dist/katex.css'
 import { GlobalStyle } from '../styles/GlobalStyle'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </>
-)
+export default ({ Component, pageProps }) => {
+  Router.events.on('routeChangeComplete', url => gtag.pageview(url))
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 import { ServerStyleSheet } from 'styled-components'
 
 type Props = {
@@ -21,6 +22,23 @@ class MyDocument extends Document<Props> {
     return (
       <Html lang="ja">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <meta charSet="utf-8" />
           {this.props.styleTags}
         </Head>
